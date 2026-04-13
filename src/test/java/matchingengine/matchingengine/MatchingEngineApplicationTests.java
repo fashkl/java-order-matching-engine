d5d7f46247552890625714b6356fb6e864e218d9
Mohamed Ali <mohamed@getbaraka.com>
2026-04-13 17:26:54 +0400

    chore: add Spring Boot application entry point and base test

---
+++ src/test/java/matchingengine/matchingengine/MatchingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package matchingengine.matchingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
